6b27e5bbb9c9a54ec6edaf9c3ad5c7e8daba653a
OfriPC\OfriG <ofri.gutman@outlook.co.il>
2023-07-08 15:54:56 +0300

    very much

---
+++ Assets/CollectiblesManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectiblesManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject[] collectibles;
+    void OnEnable()
+    {
+        DebrisMovement.OnDebrisCrash += summonCollectible;
+    }
+
+    void OnDisable()
+    {
+        DebrisMovement.OnDebrisCrash -= summonCollectible;
+    }
+
+    void summonCollectible(Vector3 position)
+    {
+        GameObject toSummon = collectibles[UnityEngine.Random.Range(0, collectibles.Length - 1)];
+        Instantiate(toSummon, position, toSummon.transform.rotation);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
