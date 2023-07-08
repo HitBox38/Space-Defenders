2a6108d672611f1619aa506151e74e1661e1f934
TomerNorman <35326023+HitBox38@users.noreply.github.com>
2023-07-08 11:38:57 +0300

    added 180 radial fill amount

--- Assets/Scripts/Managers/UI.cs
+++ Assets/Scripts/Managers/UI.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UI : MonoBehaviour
 {
+    [SerializeField] private Image fuelClock;
+    [SerializeField] private float fuelReducer = 0.05f;
+    [SerializeField] private TMP_Text currency;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currency.text = 0.ToString();
+        StartCoroutine(ReduceFillAmountOverTime());
     }
 
-    // Update is called once per frame
-    void Update()
+    // Coroutine to reduce fillAmount over time
+    IEnumerator ReduceFillAmountOverTime()
     {
-        
+        while (fuelClock.fillAmount > 0)
+        {
+            fuelClock.fillAmount -= fuelReducer * Time.deltaTime;
+            if (fuelClock.fillAmount == .75f)
+            {
+                Debug.Log("at 75%");
+            }
+            else if (fuelClock.fillAmount == .5f)
+            {
+                Debug.Log("at 50%");
+            }
+            else if (fuelClock.fillAmount == .25f)
+            {
+                Debug.Log("at 25%");
+            }
+            yield return null;
+        }
     }
 }
