925246df483aafee97dc8094d72f4bbeed61ef9d
Adi Zahavi <adidizahavi5@gmail.com>
2023-07-08 13:45:46 +0300

    HUD 1

--- Assets/Scripts/Managers/UI.cs
+++ Assets/Scripts/Managers/UI.cs
@@ -6,10 +6,16 @@ using TMPro;
 
 public class UI : MonoBehaviour
 {
+    [Header("Fuel UI")]
     [SerializeField] private Image fuelClock;
     [SerializeField] private float fuelReducer = 0.05f;
+    [Header("Currency UI")]
     [SerializeField] private TMP_Text currency;
+    [Header("UI Open Speed")]
+    [SerializeField] private float UIOpenSpeed = 5;
 
+    private Coroutine unitsMoveCo;
+    private Vector3 UISreenPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,5 +43,31 @@ public class UI : MonoBehaviour
             }
             yield return null;
         }
+    }
+
+    public void PopUpUI(GameObject toMove)
+    {
+        if(unitsMoveCo == null)
+            unitsMoveCo = StartCoroutine(MoveUnitsUI(toMove.transform, UISreenPosition));
+    }
+
+    public void SetPopUpPos(GameObject obj)
+    {
+        UISreenPosition = obj.transform.localPosition;
+    }
+
+    private IEnumerator MoveUnitsUI(Transform toMove, Vector3 position)
+    {
+        while(!V3AlmostEqual(toMove.localPosition, position, 0.005f))
+        {
+            yield return new WaitForEndOfFrame();
+            toMove.localPosition = Vector3.Lerp(toMove.localPosition, position, UIOpenSpeed * Time.deltaTime);
+        }
+        unitsMoveCo = null;
+    }
+
+    private bool V3AlmostEqual(Vector3 a, Vector3 b, float comparisonNumber)
+    {
+        return Vector3.SqrMagnitude(a - b) < comparisonNumber;
     }
 }
