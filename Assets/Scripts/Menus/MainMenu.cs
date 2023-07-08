f42233b301e57e0aafd1283ccc942ee50a1c3abd
Adi Zahavi <adidizahavi5@gmail.com>
2023-07-08 23:56:16 +0300

    MainMenu 4

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -5,6 +5,27 @@ using UnityEngine.SceneManagement;
 using TMPro;
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private RectTransform objToRotate;
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private float rotSpeed = 25;
+    [SerializeField] private float loadingTime = 5f;
+
+    private void Start()
+    {
+        StartCoroutine(LoadScreenVanish());
+    }
+
+    private void Update()
+    {
+        objToRotate.localEulerAngles -= Vector3.forward * Time.deltaTime * rotSpeed;
+    }
+
+    private IEnumerator LoadScreenVanish()
+    {
+        yield return new WaitForSeconds(loadingTime);
+        loadingScreen.SetActive(false);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
